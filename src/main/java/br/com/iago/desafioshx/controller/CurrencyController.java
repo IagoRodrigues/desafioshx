ae038514c95d093a04289ad75b5479e09cfb2aba
IagoRodrigues <rodrigues-iago@hotmail.com>
2021-08-13 11:54:03 -0300

    Criação
    Criação do projeto começando pela API. Usei spring initializr para dar início e criei dois métodos simples para testes e validações

--- src/main/java/br/com/iago/desafioshx/controller/CurrencyController.java
+++ src/main/java/br/com/iago/desafioshx/controller/CurrencyController.java
@@ -4,13 +4,17 @@ import java.sql.Timestamp;
 import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import br.com.iago.desafioshx.model.Moeda;
 
+@Controller
 public class CurrencyController {
 	
 	@RequestMapping("/listarTodos")
+	@ResponseBody
 	public List<Moeda> listarMoedas() {
 		Moeda moeda1 = new Moeda(1.00, new Timestamp(System.currentTimeMillis()));
 		Moeda moeda2 = new Moeda(2.00, new Timestamp(System.currentTimeMillis()));
@@ -18,5 +22,4 @@ public class CurrencyController {
 		
 		return Arrays.asList(moeda1, moeda2, moeda3);
 	}
-
 }
